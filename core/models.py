c50c5e44e5b4a473101b85ee49e2765bfc5885de
muradaghazade <xomurad41@gmail.com>
2025-10-04 12:53:42 +0400

    m

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    short_description = models.TextField()
+    long_description = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Partner(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='partner_logos/')
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    position = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} <{self.email}>"
